0e53040102029c94ed5d7386a8a16ab0bd6f4970
Math932 <mvilbouxduplessis@gmail.com>
2026-05-15 23:18:29 +0200

    add Exo3.4

--- Revision_partiel/L1/Exo3.ml
+++ Revision_partiel/L1/Exo3.ml
@@ -32,4 +32,52 @@ let () =
         Printf.printf "peek = %d\n" (peek p)
 
 (*---*)
-let array_valide (a:int array) : bool = Array.length a != 0
+let check_non_vide t =
+  if Array.length t = 0
+  then invalid_arg "tableau vide"
+
+let minimum t =
+  check_non_vide t ;
+  let m = ref t.(0) in
+  for i = 1 to (Array.length t) -1  do
+    if (t.(i) < !m) then m := t.(i)
+  done ; m
+
+let macimum t =
+  check_non_vide t ;
+  let m = ref t.(0) in
+  for i = 1 to (Array.length t) -1  do
+    if (t.(i) >= !m) then m := t.(i)
+  done ; m
+
+
+let somme t =
+  check_non_vide t ;
+  let acc = ref 0 in
+  for i = 0 to (Array.length t ) -1 do
+    acc := !acc + t.(i)
+  done ; !acc
+
+let moyenne t =
+  check_non_vide t;
+  float_of_int (somme t) /. float_of_int (Array.length t)
+
+
+let tri_insertion (t:int array) =
+  let n = Array.length t in
+  for i = 1 to (n-1) do
+    let cle = t.(i) in
+    let j = ref (i - 1) in
+    while !j >= 0 && t.(!j) > cle do
+    t.(!j + 1) <- t.(!j) ;
+    decr j ;
+    done ;
+    t.(!j + 1) <- cle
+  done
+
+
+let () =
+  let t = [|5; 3; 8; 1; 4|] in
+  tri_insertion t;
+  Array.iter (Printf.printf "%d ") t
+  (* 1 3 4 5 8 *)
